c3576b5268ba1dfeffdd05ee9a5923b5c3882a69
pgeovanny <geovanny9@gmail.com>
2025-07-10 22:14:32 -0300

    Update extractText.js

--- backend/utils/extractText.js
+++ backend/utils/extractText.js
@@ -1,12 +1,14 @@
 import mammoth from 'mammoth';
 
 export default async function extractText(buffer, mimetype) {
-  if (mimetype === 'application/pdf') {
+  if (mimetype === 'application/pdf' || !mimetype) {
     const { default: pdfParse } = await import('pdf-parse/lib/pdf-parse.js');
     const data = await pdfParse(buffer);
     return data.text;
+  } else if (mimetype.includes('wordprocessingml')) {
+    const { value } = await mammoth.extractRawText({ buffer });
+    return value;
   } else {
-    const result = await mammoth.extractRawText({ buffer });
-    return result.value;
+    return buffer.toString('utf8');
   }
 }
